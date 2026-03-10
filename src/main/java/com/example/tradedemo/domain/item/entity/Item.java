76036a47db64ec84c7f3ce0cde5d49923c62648c
imprity <imprity041@gmail.com>
2026-03-10 13:02:59 +0900

    feat: Item entity에 필요 column 추가

--- src/main/java/com/example/tradedemo/domain/item/entity/Item.java
+++ src/main/java/com/example/tradedemo/domain/item/entity/Item.java
@@ -1,7 +1,10 @@
 package com.example.tradedemo.domain.item.entity;
 
 import com.example.tradedemo.common.entity.Base;
+import com.example.tradedemo.domain.item.enums.ItemType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,4 +18,27 @@ public class Item extends Base {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    /**
+     * 아이템 이름
+     */
+    @NotNull
+    @Column(nullable = false, length = 64)
+    private String name;
+
+    /**
+     * 아이템 타입
+     */
+    @NotNull
+    @Column(nullable = false)
+    private ItemType itemType;
+
+    public static Item create(String name, ItemType type) {
+        Item item = new Item();
+
+        item.name = Objects.requireNonNull(name);
+        item.itemType = Objects.requireNonNull(type);
+
+        return item;
+    }
 }
