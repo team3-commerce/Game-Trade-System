4ce960206274b6da0ee0f9eed2375f351fd8533b
perfect-Bee <joonslee3@gmail.com>
2026-03-10 18:10:27 +0900

    feat : 내 지갑 조회

---
+++ src/main/java/com/example/tradedemo/domain/wallet/service/WalletService.java
@@ -0,0 +1,28 @@
+package com.example.tradedemo.domain.wallet.service;
+
+import com.example.tradedemo.domain.wallet.dto.response.WalletResponse;
+import com.example.tradedemo.domain.wallet.entity.Wallet;
+import com.example.tradedemo.domain.wallet.repository.WalletRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class WalletService {
+
+    private final WalletRepository walletRepository;
+
+    /**
+     * 내 지갑 조회
+     */
+    @Transactional(readOnly = true)
+    public WalletResponse getMyWallet(Long memberId){
+
+        Wallet wallet = walletRepository.findByMemberId(memberId)
+                .orElseThrow();
+
+        return WalletResponse.of(wallet);
+    }
+
+}
