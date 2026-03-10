ea13dd03eb60bff8d8e6fe36b3202b9780ff920e
hyu1335 <hyuham1335@gmail.com>
2026-03-10 19:51:44 +0900

    fix: marketRepository 필드에 final 추가

--- src/main/java/com/example/tradedemo/domain/marketlistings/service/MarketListingService.java
+++ src/main/java/com/example/tradedemo/domain/marketlistings/service/MarketListingService.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class MarketListingService {
-    private MarketListingRepository marketListingRepository;
+    private final MarketListingRepository marketListingRepository;
 
     @Transactional(readOnly = true)
     public Page<SearchAllMarketListingResponse> getAllMarketListing(
