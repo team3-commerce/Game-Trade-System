2adc4fc3b052f1ef31f142ebe8885ddc9ac55d74
김동진 <kimdj9790@naver.com>
2026-03-10 14:01:13 +0900

    feat: JWT 도구 및 Security 기본 설정

---
+++ src/main/java/com/example/tradedemo/auth/provider/JwtTokenProvider.java
@@ -0,0 +1,75 @@
+package com.example.tradedemo.auth.provider;
+
+import com.example.tradedemo.auth.config.JwtProperties;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private final JwtProperties jwtProperties;
+    private final UserDetailsService userDetailsService;
+    private SecretKey key;
+
+    @PostConstruct
+    protected void init() {
+        this.key = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8));
+    }
+
+    /**
+     * 토큰 생성
+     */
+    public String createToken(String email, String role) {
+        Claims claims = Jwts.claims().subject(email).add("role", role).build();
+        Date now = new Date();
+
+        return Jwts.builder()
+                .claims(claims)
+                .issuedAt(now)
+                .expiration(new Date(now.getTime() + jwtProperties.getTokenValidityInSeconds() * 1000))
+                .signWith(key)
+                .compact();
+    }
+
+    /**
+     * 권한 정보 조회
+     */
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserEmail(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getUserEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    /**
+     * 토큰 유효성 검증
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
