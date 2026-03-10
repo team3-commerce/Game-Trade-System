7e241ea3b13977527aaa8b0aaca187887bdce3b2
김동진 <kimdj9790@naver.com>
2026-03-10 13:19:11 +0900

    build: spring security and jwt 의존성 추가

---
+++ src/main/java/com/example/tradedemo/auth/config/JwtProperties.java
@@ -0,0 +1,16 @@
+package com.example.tradedemo.auth.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+    private String header;
+    private String secret;
+    private Long tokenValidityInSeconds;
+}
