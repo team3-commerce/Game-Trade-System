c4d9817f06625ac940881ab76c415c4f9320a65f
imprity <imprity041@gmail.com>
2026-03-11 09:45:01 +0900

    feat: 아이템 도감 전체 조회 구현
    이때 itemType을 이용해 필터링 을 할 수 있게 구현하였고
    또 keyword를 이용한 검색도 구현하였습니다.
    
    제대로 된 검색은 아니지만 시작이라고 생각합니다.

--- src/main/java/com/example/tradedemo/domain/item/service/ItemService.java
+++ src/main/java/com/example/tradedemo/domain/item/service/ItemService.java
@@ -1,10 +1,13 @@
 package com.example.tradedemo.domain.item.service;
 
 import com.example.tradedemo.domain.item.dto.GetItemResponse;
+import com.example.tradedemo.domain.item.dto.SearchItemRequest;
 import com.example.tradedemo.domain.item.entity.Item;
 import com.example.tradedemo.domain.item.exception.ItemNotFoundException;
 import com.example.tradedemo.domain.item.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PagedModel;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,5 +19,10 @@ public class ItemService {
         Item item = itemRepository.findById(itemId).orElseThrow(() -> new ItemNotFoundException());
 
         return GetItemResponse.of(item);
+    }
+
+    public PagedModel<GetItemResponse> getManyItems(SearchItemRequest req) {
+        Page<Item> items = itemRepository.searchItem(req);
+        return new PagedModel<GetItemResponse>(items.map(GetItemResponse::of));
     }
 }
