2a257eb3afbdf0242665687da38bd030dc858a1a
hyu1335 <hyuham1335@gmail.com>
2026-03-11 12:03:22 +0900

    feat: 내 인벤토리 전체 조회, 내 인벤토리 단건 조회 기능 추가

---
+++ src/main/java/com/example/tradedemo/domain/members/service/MemberItemService.java
@@ -0,0 +1,32 @@
+package com.example.tradedemo.domain.members.service;
+
+import com.example.tradedemo.domain.members.dto.response.GetAllMemberItemResponse;
+import com.example.tradedemo.domain.members.dto.response.GetMemberItemResponse;
+import com.example.tradedemo.domain.members.exception.MemberItemNotFoundException;
+import com.example.tradedemo.domain.members.repository.MemberItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberItemService {
+
+    private final MemberItemRepository memberItemRepository;
+
+    @Transactional(readOnly = true)
+    public Page<GetAllMemberItemResponse> getAllMemberItem(Long memberId, Pageable pageable) {
+
+        return memberItemRepository.findAllMemberItemByMemberId(memberId, pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public GetMemberItemResponse getMemberItem(Long memberId, Long memberItemId) {
+
+        return memberItemRepository
+                .findMemberItemByMemberIdAndMemberItemId(memberId, memberItemId)
+                .orElseThrow(MemberItemNotFoundException::new);
+    }
+}
