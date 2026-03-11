fdcbd4a677128a13bbaf09916047f8d61dee6893
hyu1335 <hyuham1335@gmail.com>
2026-03-11 12:14:00 +0900

    fix: 로그인한 유저의 인증정보를 기준으로 memberId 를 얻도록 수정

--- src/main/java/com/example/tradedemo/domain/order/controller/OrderController.java
+++ src/main/java/com/example/tradedemo/domain/order/controller/OrderController.java
@@ -1,9 +1,11 @@
 package com.example.tradedemo.domain.order.controller;
 
+import com.example.tradedemo.auth.dto.PrincipalDetails;
 import com.example.tradedemo.domain.order.dto.response.*;
 import com.example.tradedemo.domain.order.service.OrderService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,7 +19,9 @@ public class OrderController {
      * 상품 구매
      */
     @PostMapping("/market-listings/{marketListingId}")
-    public void purchase(@PathVariable Long marketListingId, @RequestParam Long memberId) {
+    public void purchase(
+            @PathVariable Long marketListingId, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Long memberId = principalDetails.getMember().getId();
         orderService.purchase(memberId, marketListingId);
     }
 
@@ -25,7 +29,8 @@ public class OrderController {
      * 내 구매 내역 조회
      */
     @GetMapping("/me/purchases")
-    public List<TransactionResponse> getMyBuyer(@RequestParam Long memberId) {
+    public List<TransactionResponse> getMyBuyer(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Long memberId = principalDetails.getMember().getId();
         return orderService.getMyBuyer(memberId);
     }
 
@@ -33,7 +38,8 @@ public class OrderController {
      * 내 판매 내역 조회
      */
     @GetMapping("/me/sales")
-    public List<TransactionResponse> getMySeller(@RequestParam Long memberId) {
+    public List<TransactionResponse> getMySeller(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Long memberId = principalDetails.getMember().getId();
         return orderService.getMySeller(memberId);
     }
 }
