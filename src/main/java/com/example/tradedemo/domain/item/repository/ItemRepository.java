6c674217929b0d596aad1bb9c32b383cdb085023
perfect-Bee <joonslee3@gmail.com>
2026-03-11 15:24:50 +0900

    feat : 상품등록(임시)

--- src/main/java/com/example/tradedemo/domain/item/repository/ItemRepository.java
+++ src/main/java/com/example/tradedemo/domain/item/repository/ItemRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {
+public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom{
     Optional<Item> findByName(String name);
 }
