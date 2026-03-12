777377b00a789aad3ff63f41014752b2f3d188e2
imprity <imprity041@gmail.com>
2026-03-12 16:36:01 +0900

    chore: 안쓰는 import문 제거

--- src/main/java/com/example/tradedemo/domain/item/service/ItemService.java
+++ src/main/java/com/example/tradedemo/domain/item/service/ItemService.java
@@ -6,7 +6,6 @@ import com.example.tradedemo.domain.item.entity.Item;
 import com.example.tradedemo.domain.item.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.web.PagedModel;
 import org.springframework.stereotype.Service;
 import com.example.tradedemo.common.dto.PageResponse;
 import com.example.tradedemo.common.exception.ServiceException;
