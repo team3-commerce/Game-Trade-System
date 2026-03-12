9e04d395d4eb101ac19fa47cf42f1f5221f29f90
perfect-Bee <joonslee3@gmail.com>
2026-03-12 10:38:25 +0900

    fix : 상품 등록(올리면서 누락된 부분 채우기)

--- src/main/java/com/example/tradedemo/domain/marketlistings/controller/MarketListingController.java
+++ src/main/java/com/example/tradedemo/domain/marketlistings/controller/MarketListingController.java
@@ -15,7 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class MarketListingController {
