0a434e7a2d0de04d559c8ba3e8231c481af55271
perfect-Bee <joonslee3@gmail.com>
2026-03-12 10:45:43 +0900

    fix : 상품 등록(올리면서 누락된 부분 채우기)

--- src/main/java/com/example/tradedemo/domain/marketlistings/service/MarketListingService.java
+++ src/main/java/com/example/tradedemo/domain/marketlistings/service/MarketListingService.java
@@ -19,12 +19,12 @@ import com.example.tradedemo.domain.members.repository.MemberItemRepository;
 import com.example.tradedemo.domain.members.repository.MemberRepository;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
