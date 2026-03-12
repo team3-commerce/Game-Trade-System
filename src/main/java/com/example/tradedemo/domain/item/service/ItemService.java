726d8f405d69fa4907a86672a53e24063401d3d5
imprity <imprity041@gmail.com>
2026-03-12 16:26:13 +0900

    refactor: ServiceException을 쓰도록 변경
    저희가 상의했던 대로 ServiceException을 쓰도록 변경하였습니다.

--- src/main/java/com/example/tradedemo/domain/item/service/ItemService.java
+++ src/main/java/com/example/tradedemo/domain/item/service/ItemService.java
@@ -3,13 +3,14 @@ package com.example.tradedemo.domain.item.service;
 import com.example.tradedemo.domain.item.dto.GetItemResponse;
 import com.example.tradedemo.domain.item.dto.SearchItemRequest;
 import com.example.tradedemo.domain.item.entity.Item;
-import com.example.tradedemo.domain.item.exception.ItemNotFoundException;
 import com.example.tradedemo.domain.item.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PagedModel;
 import org.springframework.stereotype.Service;
 import com.example.tradedemo.common.dto.PageResponse;
+import com.example.tradedemo.common.exception.ServiceException;
+import com.example.tradedemo.common.exception.ErrorEnum;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +18,9 @@ public class ItemService {
     private final ItemRepository itemRepository;
 
     public GetItemResponse getItem(Long itemId) {
-        Item item = itemRepository.findById(itemId).orElseThrow(() -> new ItemNotFoundException());
+        Item item = itemRepository.findById(itemId).orElseThrow(
+                () -> new ServiceException(ErrorEnum.ERR_ITEM_NOT_FOUND)
+        );
 
         return GetItemResponse.of(item);
     }
