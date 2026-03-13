eba877e4756bf4246a5f8b08ee6281dcc353a066
perfect-Bee <joonslee3@gmail.com>
2026-03-13 15:46:27 +0900

    fix : @PathVariable

--- src/main/java/com/example/tradedemo/domain/pending/controller/PendingAssetController.java
+++ src/main/java/com/example/tradedemo/domain/pending/controller/PendingAssetController.java
@@ -7,10 +7,7 @@ import com.example.tradedemo.domain.pending.service.PendingAssetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -51,7 +48,7 @@ public class PendingAssetController {
     @PostMapping("/api/v1/me/pending-assets/{pendingAssetId}")
     public ResponseEntity<ApiResponse<Void>> settlement(
             @AuthenticationPrincipal PrincipalDetails principalDetails,
-            @RequestBody Long pendingAssetId
+            @PathVariable Long pendingAssetId
     ) {
 
         Long memberId = principalDetails.getMember().getId();
